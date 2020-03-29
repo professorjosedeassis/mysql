0846ef9a2e4ed182ee074b340678d26741686be0
Professor José de Assis <josedeassisfilho@gmail.com>
2020-03-29 20:55:51 -0300

    Adicionei o script da aula1

---
+++ aula1/script1.sql
@@ -0,0 +1,15 @@
+/* 
+	SQL - Aula 1
+    @author Professor José de Assis
+*/
+
+-- O comando abaixo lista os bancos "<ctrl><enter> para executar"
+show databases;
+
+-- O comando abaixo cria um novo banco de dados
+create database agenda;
+show databases;
+
+-- O comando abaixo seleciona o banco de dados
+use agenda;
+    
