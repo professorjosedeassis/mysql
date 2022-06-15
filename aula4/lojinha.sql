6535976351c4fd5122b59c17f192af2e2b5a39f0
Professor José de Assis <josedeassisfilho@gmail.com>
2022-06-15 16:57:09 -0300

    Update lojinha.sql

--- aula4/lojinha.sql
+++ aula4/lojinha.sql
@@ -1,154 +1,204 @@
 /**
-	E-Commerce
+    Lojinha
     @author Professor José de Assis
+    @version 1.2
 */
 
-create database dbloja;
-use dbloja;
+create database lojinha;
+use lojinha;
 
-create table clientes (
-	idcli int primary key auto_increment,
-    nome varchar(50) not null,
-    email varchar(50) unique not null,
-    senha varchar(250) not null
+create table usuarios (
+    idusu int primary key auto_increment,
+    usuario varchar(50) not null,
+    login varchar(10) not null unique,
+    senha varchar(255) not null,
+    perfil varchar(20) not null
 );
 
-insert into clientes (nome,email,senha)
-values ('José de Assis','jose@email.com',md5('123456'));
+describe usuarios;
 
-describe clientes;
-select * from clientes;
+insert into usuarios(usuario,login,senha,perfil)
+values('Administrador','admin',md5('admin'),'admin');
+insert into usuarios(usuario,login,senha,perfil)
+values('José de Assis','joseassis',md5('123456'),'user');
 
--- timestamp default current_timestamp (data e hora automático)
--- date (tipo de dados relacionados a data) YYYYMMDD
-create table produtos (
-	codigo int primary key auto_increment,
-    barcode varchar(50) unique,
+select * from usuarios;
+
+-- Acessando o sistema pela tela de login
+select * from usuarios where login='admin' and senha=md5('admin');
+
+create table fornecedores (
+    idfor int primary key auto_increment,
+    cnpj varchar(20) not null unique,
+    ie varchar(20) unique,
+    im varchar(20) unique,
+    razao varchar(50) not null,
+    fantasia varchar(50) not null,
+    site varchar(50),
+    fone varchar(15) not null,
+    contato varchar(50),     
+    email varchar(50),
+    cep varchar(9) not null,
+    endereco varchar(100) not null,
+    numero varchar(10) not null,
+    complemento varchar(20),
+    bairro varchar(50) not null,
+    cidade varchar(50) not null,
+    uf char(2) not null,
+    obs varchar(255)
+);
+
+insert into fornecedores
+(cnpj,razao,fantasia,fone,cep,endereco,numero,bairro,cidade,uf)
+values ('43.283.811/0001-50','Kalunga Comercio e Industria Grafica Ltda','Kalunga',
+'(11)3346-9966','03307-005','Rua Tuiuti','2516','Tatuapé','São Paulo','SP');
+
+select * from fornecedores;
+
+create table produtos(
+    codigo int primary key auto_increment,
+    barcode varchar(255) unique,
     produto varchar(100) not null,
-    fabricante varchar(100) not null,
+    descricao varchar(255) not null,
+    fabricante varchar(50) not null,
     datacad timestamp default current_timestamp,
     dataval date not null,
     estoque int not null,
     estoquemin int not null,
-    medida varchar(50) not null,
-    valor decimal(10,2),
-    loc varchar(100)
+    unidade varchar(50) not null,
+    localizacao varchar(50),
+    custo decimal(10,2) not null,
+    lucro decimal(10,2),    
+    idfor int not null,
+    foreign key(idfor) references fornecedores(idfor)
 );
 
-describe produtos;
-
-insert into produtos(produto,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
+insert into produtos(barcode,produto,descricao,fabricante,dataval,
+estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
 values
-('Caneta BIC azul','BIC',20221005,100,10,'CX',28.75,'setor A p2');
+('12345678910','Caneta BIC Azul','Caneta bic ponta fina azul','BIC',
+20240523,20,5,'UN','Prateleira 2',0.75,100,1);
 
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
+insert into produtos(barcode,produto,descricao,fabricante,dataval,
+estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
 values
-('Régua Jojo 30cm','2222222222','JJBA',20221006,50,5,'UN',3.40,'setor A p3');
+('12345678911','Caneta BIC Vermelha','Caneta bic ponta fina vermelha','BIC',
+20240525,2,5,'UN','Prateleira 3',0.75,100,1);
 
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
+insert into produtos(barcode,produto,descricao,fabricante,dataval,
+estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
 values
-('Cola bastão','3333333333','Tenaz',20210506,50,5,'CX',1,'setor B p1');
+('12345678912','Cola branca','Cola branca de papel','Tenaz',
+20210525,20,5,'UN','Prateleira 5',2,50,1);
 
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
+insert into produtos(barcode,produto,descricao,fabricante,dataval,
+estoque,estoquemin,unidade,localizacao,custo,lucro,idfor)
 values
-('Massa de modelar ABC','4444444444','Toki',20200910,10,5,'UN',4.40,'setor C p1');
-
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Lápis preto CX 20','5555555555','Presto',20221006,10,20,'CX',5,'setor A p3');
-
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Borracha branca','6666666666','pirelli',20231006,15,50,'UN',0.50,'setor A p4');
+('123456789013','Clips Galvanizados','Clips Galvanizados tamanho 2/0 100 unidades',
+'Tilibra',20270525,5,10,'CX','Prateleira 7',3.50,100,1);
 
 select * from produtos;
 
--- Inventário do estoque(total)
-select sum(estoque * valor) as total from produtos;
+-- Obter o preço de venda dos produtos
+select codigo as código,produto,custo,
+(custo + (custo * lucro)/100) as venda
+from produtos;
 
--- Relatório de reposição do estoque 1
-select * from produtos where estoque < estoquemin;
+-- Inventário do estoque (patrimônio)
+select sum(estoque * custo) as total from produtos;
 
--- Relatório de reposição do estoque 2
--- date_format() -> formatar a exibição da data
--- %d (dia) %m (mês) %y (ano 2 dígitos) %Y (ano 4 dígitos)
+-- relatório de reposição de estoque
+-- %d/%m/%Y dd/mm/aaaa | %d/%m/%y dd/mm/aa | %H:%i HH:MM
 select codigo as código,produto,
 date_format(dataval,'%d/%m/%Y') as data_validade,
 estoque, estoquemin as estoque_mínimo
 from produtos where estoque < estoquemin;
 
--- Relatório de validade dos produtos 1
-select codigo as código,produto,
-date_format(dataval,'%d/%m/%Y') as data_validade
-from produtos;
-
--- Relatório de validade dos produtos 2
--- datediff() retorna a diferença em dias de duas datas
--- curdate() data atual
-select codigo as código, produto,
+-- relatório de produtos vencidos
+select codigo as código,produto,localizacao as localização,
 date_format(dataval,'%d/%m/%Y') as data_validade,
-datediff(dataval,curdate()) as dias_restantes
-from produtos;
+datediff(dataval,curdate()) as dias_vencidos
+from produtos where datediff(dataval,curdate()) < 0;
 
--- decimal (tipo de dado numérico não inteiro 10,2 dez digitos com 2 casas decimais de precisão)
-create table pedidos(
-	pedido int primary key auto_increment,
+create table clientes (
+    idcli int primary key auto_increment,
+    nome varchar(50) not null,
+    fone varchar(15) not null,
+    cpf varchar(15) unique,
+    email varchar(50),
+    marketing char(1) not null,
+    cep varchar(9),
+    endereco varchar(100),
+    numero varchar(10),
+    complemento varchar(20),
+    bairro varchar(50),
+    cidade varchar(50),
+    uf char(2)
+);
+
+insert into clientes(nome,fone,marketing)
+values('Kelly Cristina','99999-1234','n');
+
+insert into clientes(nome,fone,email,marketing)
+values('Sirlene Sanches','99999-4321','sisa@email.com','s');
+
+-- E-mail marketing
+select nome as Cliente,email as E_mail from clientes where marketing='s';
+
+create table pedidos (
+    pedido int primary key auto_increment,
     dataped timestamp default current_timestamp,
     total decimal(10,2),
     idcli int not null,
     foreign key(idcli) references clientes(idcli)
 );
 
+-- abertura de um pedido
 insert into pedidos(idcli) values(1);
-select * from pedidos;
 
--- abertura do pedido
+-- verificar pedidos abertos
 select
 pedidos.pedido,
 date_format(pedidos.dataped,'%d/%m/%Y - %H:%i') as data_pedido,
-clientes.nome as cliente, clientes.email as e_mail
+clientes.nome as cliente,
+clientes.fone
 from pedidos inner join clientes
 on pedidos.idcli = clientes.idcli;
 
+-- Tabela de apoio para criar um relacionamento do tipo N-N
 create table carrinho (
-	pedido int not null,
+    pedido int not null,
     codigo int not null,
     quantidade int not null,
     foreign key(pedido) references pedidos(pedido),
     foreign key(codigo) references produtos(codigo)
 );
 
+insert into carrinho values (1,1,2);
+insert into carrinho values (1,4,1);
+
 select * from carrinho;
 
-insert into carrinho (pedido,codigo,quantidade)
-values (1,1,10);
-insert into carrinho (pedido,codigo,quantidade)
-values (1,2,5);
-
--- exibir o carrinho
-select
-pedidos.pedido,
+-- Exibir o carrinho
+select pedidos.pedido,
 carrinho.codigo as código,
 produtos.produto,
 carrinho.quantidade,
-produtos.valor,
-produtos.valor * carrinho.quantidade as sub_total
+produtos.custo + (custo*lucro/100) as valor,
+(produtos.custo + (custo*lucro/100)) * carrinho.quantidade as sub_total
 from (carrinho inner join pedidos on carrinho.pedido = pedidos.pedido)
 inner join produtos on carrinho.codigo = produtos.codigo;
 
--- total do carrinho
-select sum(produtos.valor * carrinho.quantidade) as total
-from carrinho inner join produtos on carrinho.codigo = produtos.codigo;
+-- total do pedido
+select sum((produtos.custo + (custo*lucro/100)) * carrinho.quantidade) as total
+from carrinho inner join produtos
+on carrinho.codigo = produtos.codigo;
 
--- atualizar o estoque após o fechamento do pedido
+-- atualização do estoque (fechamento do pedido)
 update carrinho
 inner join produtos
 on carrinho.codigo = produtos.codigo
 set produtos.estoque = produtos.estoque - carrinho.quantidade
 where carrinho.quantidade > 0;
+
+select * from produtos;
